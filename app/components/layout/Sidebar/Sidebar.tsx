fb0b9cb0f34034c62e2098932f86d0d02c290917
eggnom <h75408706@gmail.com>
2023-02-05 16:24:51 +0300

    layout

---
+++ app/components/layout/Sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+import styles from "./Sidebar.module.scss"
+
+const Sidebar: FC = () => {
+    return (
+        <div className={styles.sidebar}>
+            Sidebar
+        </div>
+    )
+}
+
+export default Sidebar
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import '../app/assets/styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
