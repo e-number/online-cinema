c2ed2248d7bee55d98cbbf9f15e9ae5be9b6775f
eggnom <h75408706@gmail.com>
2023-02-19 22:00:56 +0300

    add redux toolkit

--- app/providers/MainProvider.tsx
+++ app/providers/MainProvider.tsx
@@ -1,13 +1,13 @@
 // import AuthProvider from './AuthProvider/AuthProvider'
-// import HeadProvider from './HeadProvider/HeadProvider'
+import HeadProvider from './HeadProvider/HeadProvider'
 import { FC, PropsWithChildren } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Provider } from 'react-redux'
-// import { store } from 'store/store'
+import { store } from 'store/store'
 
 import Layout from '@/components/layout/Layout'
 
-// import ReduxToastr from '@/ui/redux-toastr/ReduxToastr'
+import ReduxToastr from '@/ui/redux-toastr/ReduxToastr'
 
 import { TypeComponentAuthFields } from '@/shared/types/auth.types'
 
@@ -21,20 +21,20 @@ const queryClient = new QueryClient({
 
 const MainProvider: FC<PropsWithChildren> = ({ children }) => {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<Layout>{children}</Layout>
-		</QueryClientProvider>
+		// <QueryClientProvider client={queryClient}>
+		// 	<Layout>{children}</Layout>
+		// </QueryClientProvider>
 
-		// <HeadProvider>
-		// 	<Provider store={store}>
-		// 		<QueryClientProvider client={queryClient}>
-		// 			<ReduxToastr />
-		// 			<AuthProvider Component={Component}>
-		// 				<Layout>{children}</Layout>
-		// 			</AuthProvider>
-		// 		</QueryClientProvider>
-		// 	</Provider>
-		// </HeadProvider>
+		<HeadProvider>
+			<Provider store={store}>
+				<QueryClientProvider client={queryClient}>
+					<ReduxToastr />
+					{/* <AuthProvider Component={Component}> */}
+					<Layout>{children}</Layout>
+					{/* </AuthProvider> */}
+				</QueryClientProvider>
+			</Provider>
+		</HeadProvider>
 	)
 }
 
