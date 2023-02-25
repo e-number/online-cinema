13af31c2b9269cf1e2936f8706fa05c1b7175d9d
eggnom <h75408706@gmail.com>
2023-02-25 23:29:55 +0300

    favorite fix

--- app/components/layout/Sidebar/MoviesContainer/FavoriteMovieList/FavoriteMovieList.tsx
+++ app/components/layout/Sidebar/MoviesContainer/FavoriteMovieList/FavoriteMovieList.tsx
@@ -1,23 +1,32 @@
 import { FC } from 'react'
 
-// import { useFavorites } from '@/components/screens/favorites/useFavorites'
+import { useFavorites } from '@/components/screens/favorites/useFavorites'
 import SkeletonLoader from '@/components/ui/skeleton-loader/SkeletonLoader'
 
-// import { useAuth } from '@/hooks/useAuth'
+import { useAuth } from '@/hooks/useAuth'
 
 import MovieList from '../MovieList'
 
 import NotAuthFavorites from './NotAuthFavorites'
 
 const FavoriteMovieList: FC = () => {
-	// const { isLoading, favoritesMovies } = useFavorites()
-	// const { user } = useAuth()
+	const { isLoading, favoritesMovies } = useFavorites()
+	const { user } = useAuth()
 
-	// if (!user) return <NotAuthFavorites />
+	if (!user) return <NotAuthFavorites />
 
-	// 
-	return (
-		<div>FavoriteMovieList</div>
+	return isLoading ? (
+		<div className="mt-11">
+			<SkeletonLoader count={3} className="h-28 mb-4" />
+		</div>
+	) : (
+		<MovieList
+			list={{
+				link: '/favorites',
+				movies: favoritesMovies?.slice(0, 3) || [],
+				title: 'Favorites',
+			}}
+		/>
 	)
 }
 
